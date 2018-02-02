ecc28d28c7121b3f7f0d3867f52d28de4b483160
Nick Kolesnikov <dev.nick.kolesnikov@gmail.com>
2018-02-02 14:57:50 +0200

    Initial commit

---
+++ install/SQL/php_simple_blog.sql
@@ -0,0 +1,28 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for comments
+-- ----------------------------
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `post_id` text,
+  `author` varchar(32) DEFAULT NULL,
+  `text` text,
+  `datetime` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for posts
+-- ----------------------------
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `author` varchar(32) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `text` text,
+  `datetime` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+SET FOREIGN_KEY_CHECKS=1;
